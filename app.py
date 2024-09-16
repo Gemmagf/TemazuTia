fbd8b6374aa7f10807cd6dd391cbd3287d58523d
Gemmagf <65533478+Gemmagf@users.noreply.github.com>
2024-09-16 16:33:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-
-st.markdown("<h1 style='text-align: center;font-family:Ovo; color: #C874B2;'>Capital de Carrera\n", unsafe_allow_html=True)
-st.write("# ")
+st.title("Hello World!")
+st.write("This is a simple Streamlit app.")
